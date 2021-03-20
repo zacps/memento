5c7aeaeb7e49961b366045141cfcbf6a99fd3bb8
NeedsSoySauce <feras.albaroudi@gmail.com>
2021-03-20 23:55:52 +1300

    Add test cases for other callable types

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -21,7 +21,7 @@ def test_parallel_uses_multiple_processes():
     manager.add_tasks((delayed(get_process_id)() for _ in range(10)))
     results = manager.run()
 
-    assert len(set(results)) == len(results)
+    assert len(set(results)) > 1
 
 
 def test_parallel_uses_correct_number_of_processes():
@@ -30,7 +30,26 @@ def test_parallel_uses_correct_number_of_processes():
     manager.add_tasks((delayed(get_process_id)() for _ in range(10)))
     results = manager.run()
 
-    assert len(set(results)) == 5
+    assert len(set(results)) <= 5
+
+
+class TestClass:
+    def __init__(self, x: int, y: int):
+        self._result = x + y
+
+    def __eq__(self, other):
+        return isinstance(other, TestClass) and self._result == other._result
+
+
+class CallableTestClass:
+    def __call__(self, x: int, y: int):
+        return x + y
+
+
+def recursive_function(x: int):
+    if x <= 0:
+        return 0
+    return 1 + recursive_function(x-1)
 
 
 @pytest.mark.parametrize(
@@ -39,6 +58,9 @@ def test_parallel_uses_correct_number_of_processes():
         (delayed(sum)([1, 2]), [3]),
         (delayed(lambda x: sum(x))([2, 2]), [4]),
         (delayed(functools.partial(lambda x, y: sum([x, y]), 2))(3), [5]),
+        (delayed(TestClass)(3, 3), [TestClass(3, 3)]),
+        (delayed(CallableTestClass())(3, 4), [7]),
+        (delayed(recursive_function)(8), [8]),
     ],
 )
 def test_parallel_returns_correct_result(task: Callable, expected: List):
