a3a7230484c1dc7c85051e76cf044c53157aba9e
Liam Scott-Russell <liam.scott.russell@gmail.com>
2021-03-23 21:43:43 +1300

    Create initial caching implementation

---
+++ memento/caching.py
@@ -0,0 +1,69 @@
+import dill
+from typing import Callable
+from abc import ABC, abstractmethod
+
+
+class CacheProvider(ABC):
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, item):
+        return self.set(key, item)
+
+    def __contains__(self, key):
+        return self.contains(key)
+
+    @abstractmethod
+    def get(self, key):
+        pass
+
+    @abstractmethod
+    def set(self, key, item) -> None:
+        pass
+
+    @abstractmethod
+    def contains(self, key) -> bool:
+        pass
+
+    @abstractmethod
+    def make_key(self, func: Callable, *args, **kwargs) -> str:
+        pass
+
+
+class MemoryCacheProvider(CacheProvider):
+    def __init__(self, initial_cache: dict = None):
+        if initial_cache is None:
+            initial_cache = (
+                {}
+            )  # Explicitly create dictionary here, as otherwise would be externally mutable
+        self.cache = initial_cache
+
+    def get(self, key):
+        return self.cache.get(key)
+
+    def set(self, key, item) -> None:
+        self.cache[key] = item
+
+    def contains(self, key) -> bool:
+        return self.cache.get(key, False) is not False
+
+    def make_key(self, func: Callable, *args, **kwargs) -> str:
+        return dill.dumps({"function": func, "args": args, "kwargs": kwargs})
+
+
+class Cache:
+    def __init__(self, func: Callable, cache_provider: CacheProvider = None):
+        self.func = func
+        if cache_provider is None:
+            cache_provider = MemoryCacheProvider()
+        self.cache_provider = cache_provider
+
+    def __call__(self, *args, **kwargs):
+        key = self.cache_provider.make_key(self, self.func, *args, **kwargs)
+
+        if key in self.cache_provider:
+            return self.cache_provider[key]
+        else:
+            value = self.func(*args, **kwargs)  # execute the function, with arguments
+            self.cache_provider[key] = value
+            return value
