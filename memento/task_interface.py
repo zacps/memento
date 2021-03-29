6430063b22fd9706a926ad8a76f86010b2370a0f
nipun-jasti <nipunjasti@gmail.com>
2021-03-29 23:52:15 +1300

    Added studs

--- memento/task_interface.py
+++ memento/task_interface.py
@@ -1,37 +1,71 @@
 """
-Contains MEMENTO's task interface, once the configurations are generated and dispatched to tasks, we need some way to interact with the user code
+Contains MEMENTO's task interface, once the configurations are
+generated and dispatched to tasks, we need some way to interact
+with the user code
 """
-import sklearn
 
-matrix = {
-    "parameters": {
-        "model": [
-            sklearn.svm.SVC,
-            sklearn.linear_model.Perceptron,
-            sklearn.linear_model.LogisticRegression,
-        ],
-        "dataset": ["imagenet", "mnist", "cifar10", "quickdraw"],
-    }
-}
+import configurations
+
+matrix = {}
 
 
-def my_experiment(context, config):
+def experiment(context, config):
     """
-    Running the base experiment
+    Running the base experiment for all experiments
+
+    ### Example
+
+    ```python
+    results = experiment(context, config)
+    ```
+
     """
+
+    def task_input(text):
+        """
+        Takes the experiment and sends it to the tasks
+        """
+
+        return NotImplementedError
+
+    def task_output():
+        """
+        The results after the test is finished
+        """
+
+        return NotImplementedError
+
+    def collect_metrics():
+        """
+        Gets the new metrics to start next experiment
+        """
+
+        return NotImplementedError
+
+    def checkpoint():
+        """
+        Allows the user to revert the directory to any point in
+        the past that was ‘commit’ to save the changes.
+        """
+
+        return NotImplementedError
+
     model = config.model()
     dataset = get_dataset(config.dataset)
     results = []
+    text = context
+    text_checkpoint = context.checkpoint()
 
     while not len(results) > 1:
 
-        results.append(expensive_thing())
-        context.collect_metrics()
-        context.checkpoint()
+        task_input(text)
+        results.append(task_output())
+        text = collect_metrics()
+        text_checkpoint = checkpoint()
 
-    raise NotImplementedError
+    # raise NotImplementedError
 
     return aggregate(results)
 
 
-memento.run(my_experiment, matrix)
+# memento.run(experiment, matrix)
