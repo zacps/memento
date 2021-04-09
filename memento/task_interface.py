4694e49bd4b49b0093660591a42070c478d61e21
nipun-jasti <nipunjasti@gmail.com>
2021-04-09 18:08:29 +1200

    Updated task_interface

--- memento/task_interface.py
+++ memento/task_interface.py
@@ -4,6 +4,8 @@ generated and dispatched to tasks, we need some way to interact
 with the user code
 """
 
+from memento.configurations import run
+
 
 class Context:
     """
@@ -13,8 +15,10 @@ class Context:
 
     ```python
     context_example = Context()
+    context_example.results(tasks)
     context_example.collect_metrics()
     context_example.checkpoint()
+    context_example.restore(2)
     ```
     """
 
@@ -23,18 +27,34 @@ class Context:
         Initalizes the context
         """
         self.context = context
+        self.results(run(context))
+
+    def results(self, task):
+        """
+        return class from Memento.run which wild hold
+        the results of the tasks themselves and any metadata we
+        want to add
+        """
+        self.task = task
+        self.task_results = self.task.result()
+        self.metadata = self.task.metadata()
 
     def collect_metrics(self):
         """
         Gets the new metrics to start next experiment
         """
-        self.context.collect_metrics()
-        return NotImplementedError
+        return self.collect_metrics()
 
     def checkpoint(self):
         """
         Allows the user to revert the directory to any point in
         the past that was ‘commit’ to save the changes.
         """
-        self.context.checkpoint()
-        return NotImplementedError
+        return self.checkpoint()
+
+    def restore(self, checkpoint):
+        """
+        Gets the data saved at the checkpoint the user want to retrieve
+        from
+        """
+        return self.restore(checkpoint)
