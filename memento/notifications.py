3428721c8d1822b87b5c2b6a47815d034d256671
Joshua de Wet <59136536+Dewera@users.noreply.github.com>
2021-04-28 13:28:06 +1200

    Added abstract provider

---
+++ memento/notifications.py
@@ -0,0 +1,28 @@
+"""
+
+"""
+from abc import ABC, abstractmethod
+
+
+class NotificationProvider(ABC):
+    """
+
+    """
+
+    @abstractmethod
+    def job_completion(self):
+        """
+
+        """
+
+    @abstractmethod
+    def task_completion(self):
+        """
+
+        """
+
+    @abstractmethod
+    def task_failure(self):
+        """
+
+        """
