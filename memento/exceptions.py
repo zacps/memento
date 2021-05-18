cdbb99c01495397838d95fa111459114051e6fc7
Joshua de Wet <59136536+Dewera@users.noreply.github.com>
2021-05-18 14:34:34 +1200

    Added matrix graph ordering

--- memento/exceptions.py
+++ memento/exceptions.py
@@ -14,3 +14,12 @@ class CacheMiss(Exception):
 
     def __init__(self, config: Config) -> None:
         super().__init__(f"Config {config} was not found in the cache")
+
+
+class CyclicDependency(Exception):
+    """
+        Raised when ``run_all`` is called with a cyclic dependency in one or more of the matrices.
+    """
+
+    def __init__(self) -> None:
+        super().__init__(f"Cyclic dependency detected")
